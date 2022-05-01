72aa6730e58c4b58737593c1fd304cf97d82ed69
yash-2507 <99003521+yash-2507@users.noreply.github.com>
2022-05-01 09:52:13 +0530

    small changes

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../features/todo/todoSlice";
 
 export default function TodoInput() {
     const [input, setInput] = useState("");
+    const inputRef = useRef();
     const dispatch = useDispatch();
 
     return (
         <div>
             <input
+                ref={inputRef}
                 type="text"
                 placeholder="Enter ToDo"
                 onChange={(event) => {
@@ -21,6 +23,8 @@ export default function TodoInput() {
                     let toPush = { title: input };
                     // console.log("toPush: ", toPush);
                     dispatch(addTodo(toPush));
+                    inputRef.current.value = "";
+                    inputRef.current.focus();
                 }}
             >
                 Add ToDo
